1d69f520384884f685fb224e2e652bb132910592
Erik Timmermann <erikt@noao.edu>
2017-04-11 11:25:58 -0700

    erikt: Added a simple sanity check.

--- src/readnessi.c
+++ src/readnessi.c
@@ -3,14 +3,22 @@
 #include <stdio.h>
 #include "fitsio.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
     fitsfile *fptr;
     char card[FLEN_CARD];
     int status = 0, nkeys, ii;  /* MUST initialize status */
 
+    if(argc < 2)
+    {
+        fprintf(stderr, "Error: Usage %s FITS_FILE\n", argv[0]);
+	exit(0);
+    }
+
     fits_open_file(&fptr, argv[1], READONLY, &status);
     fits_get_hdrspace(fptr, &nkeys, NULL, &status);
-    for (ii = 1; ii <= nkeys; ii++) {
+    for (ii = 1; ii <= nkeys; ii++) 
+    {
         fits_read_record(fptr, ii, card, &status); /* read keyword */
         printf("%s\n", card);
     }
